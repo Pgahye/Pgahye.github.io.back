4d6dd2fd76b47777ca72f4d81c2fde136a4ef8a1
Pgahye <rkgp0515@gmail.com>
2018-01-07 15:41:39 +0900

    341

--- travis-build.php
+++ travis-build.php
@@ -1,12 +1,29 @@
-
-
-
-
-
 <?php
-// Use in the "Post-Receive URLs" section of your GitHub repo.
-if ( $_SERVER['HTTP_X_GITHUB_EVENT'] == 'push' ) {
-  shell_exec( 'PWD=$(pwd)' );
-  shell_exec( '"$PWD"/travis-build.sh' );
-}
-?>hi
+
+
+shell_exec('
+body='{
+"request": {
+"branch":"source"
+}}'
+
+
+
+
+
+
+github_token="1ceeeb78bd7e84490f9d1f378147ea55eef5b7d2"
+token="$(travis login --github-token $github_token && travis token)"
+repo="Pgahye.github.io"
+user="Pgahye"
+
+curl -s -X POST \
+ -H "Content-Type: application/json" \
+ -H "Accept: application/json" \
+ -H "Travis-API-Version: 3" \
+ -H "Authorization: token $token" \
+ -d "$body" \
+ https://api.travis-ci.org/repo/$user%2F$repo/requests
+
+');
+?>
